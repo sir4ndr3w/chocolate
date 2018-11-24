136f8955d38b17f2be263b388e463e29b222546c
sir4ndr3w <marco.za@gmx.de>
2018-11-24 15:52:06 +0100

    Profile routing in seperate file now

--- app.js
+++ app.js
@@ -1,28 +1,17 @@
 const express = require('express');
 const pool = require('./db/connection');
-const db_profiles = require('./db/db_profiles');
 const bodyParser = require('body-parser');
-const validator = require('./functions/validation');
-
 const app = express();
 const router = express.Router();
-const urlencodeParser = bodyParser.urlencoded({extended: false});
+
 /**
- * Error finden, warum Import nicht korrekt funktioniert
+ * Routing
  */
-//const profiles = require('./routes/profiles');
+const profiles = require('./routes/profiles');
 //const start = require('./routes/index');
 
 router.use(function (req, res, next) {
     console.log('Time:', Date.now());
-    next();
-});
-// a middleware sub-stack shows request info for any type of HTTP request to the /user/:id path
-router.use('/profiles/:id', function (req, res, next) {
-    console.log('Request URL:', req.originalUrl);
-    next();
-}, function (req, res, next) {
-    console.log('Request Type:', req.method);
     next();
 });
 
@@ -40,86 +29,7 @@ pool.on('release', function (connection) {
 app.set('view engine', 'ejs');
 app.use(bodyParser());
 
-/**
- * Routing
- */
-router.get('/profiles/get/:id', function (req, res, next) {
-    res.header("Content-Type", 'application/json');
-    let foundData = false;
-    if (Number.isInteger(parseInt(req.params.id))) {
-        db_profiles.getUserById(req.params.id, (err, data) => {
-            if (err) throw err;
-
-            if(data){
-                foundData = true;
-                res.send(JSON.stringify(data, null, 5));
-            }
-        });
-    }
-    if(!foundData){
-        next('route');
-    }
-});
-
-router.post('/profiles/post/', urlencodeParser, function (req, res, next) {
-    let escapedInput = null;
-    let userAdded = false;
-    validator.registerUser(req.body, (err, data) => {
-        if(err === null) {
-            escapedInput = data;
-
-            if (escapedInput[0]) {
-                db_profiles.isEmailInUse(escapedInput[1], (err, emailIsInUse) => {
-                    if (err) throw err;
-
-                    if (!emailIsInUse) {
-                        db_profiles.insertNewUser(escapedInput, (err, isInserted) => {
-                            if (err) throw err;
-                            if (isInserted) {
-                                userAdded = true;
-                                db_profiles.getNewUserId((err, result) => {
-                                    res.redirect('../get/' + result);
-                                });
-                            }
-                        });
-                    }
-                });
-            }
-        }
-    });
-    if(!userAdded){
-        next('route');
-    }
-});
-
-/**
- * kein Benutzer gefunden
- */
-router.get('/profiles/get/:id', function (req, res, next) {
-    res.header("Content-Type", 'application/json');
-    res.status(404);
-    res.send(JSON.stringify({error: 'Benutzer existiert nicht.'}, 0, 5));
-});
-
-/**
- * falscher Input
- */
-router.post('/profiles/post/', function (req, res, next) {
-    res.header("Content-Type", 'application/json');
-    res.status(404);
-    res.send(JSON.stringify({
-        error: 'Benutzer konnte nicht angelegt werden.',
-        requirements: ">8 character, 1 Upper Case, 1 Lower Case, 1 Digit, 1 Sign",
-        warning: "Your e-mail address may be in use already."
-    }, 0, 5));
-});
-
-/**
- * Interface zum POST testen
- */
-router.get('/profiles/post/', function (req, res) {
-    res.sendFile(__dirname + "/tester.html");
-});
+router.use('/profiles/', profiles);
 
 /**
  * API Informationen
--- routes/profiles.js
+++ routes/profiles.js
@@ -1,8 +1,107 @@
 const express = require('express');
+const db_profiles = require('../db/db_profiles');
+const bodyParser = require('body-parser');
+const validator = require('../functions/validation');
 const router = express.Router();
+const urlencodeParser = bodyParser.urlencoded({extended: false});
 
-router.get('/', function(req,res,next){
-    res.send("special");
+/**
+ * Benutzer ausgeben
+ */
+// a middleware sub-stack shows request info for any type of HTTP request to the /user/:id path
+router.use(function (req, res, next) {
+    console.log('Request URL:', req.originalUrl);
+    next();
+}, function (req, res, next) {
+    console.log('Request Type:', req.method);
+    next();
+});
+
+/**
+ * Benutzerdaten laden
+ */
+router.get('/get/:id', function (req, res, next) {
+    res.header("Content-Type", 'application/json');
+    if (Number.isInteger(parseInt(req.params.id))) {
+        db_profiles.getUserById(req.params.id, (err, data) => {
+            if (err) throw err;
+
+            console.log(data);
+
+            if(data){
+                res.send(JSON.stringify(data, null, 5));
+            } else {
+                next('route');
+            }
+        });
+    } else {
+        next('route');
+    }
+});
+
+/**
+ * kein Benutzer gefunden
+ */
+router.get('/get/:id', function (req, res, next) {
+    res.header("Content-Type", 'application/json');
+    res.status(404);
+    res.send(JSON.stringify({error: 'Benutzer existiert nicht.'}, 0, 5));
+});
+
+/**
+ * Interface zum POST testen
+ */
+router.get('/post/', function (req, res) {
+    res.sendFile(__dirname + "/tester.html");
+});
+
+/**
+ * Neuen Benutzer hinzufügen
+ */
+router.post('/post/', urlencodeParser, function (req, res, next) {
+    res.header("Content-Type", 'application/json');
+    let escapedInput = null;
+    validator.registerUser(req.body, (err, data) => {
+        if(err === null) {
+            escapedInput = data;
+
+            if (escapedInput[0]) {
+                db_profiles.isEmailInUse(escapedInput[1], (err, emailIsInUse) => {
+                    if (err) throw err;
+
+                    if (!emailIsInUse) {
+                        db_profiles.insertNewUser(escapedInput, (err, isInserted) => {
+                            if (err) throw err;
+                            if (isInserted) {
+                                db_profiles.getNewUserId((err, result) => {
+                                    res.redirect('../get/' + result);
+                                });
+                            } else {
+                                next('route');
+                            }
+                        });
+                    } else {
+                        next('route');
+                    }
+                });
+            }
+        } else {
+            next('route');
+        }
+    });
+});
+
+/**
+ * falscher Input
+ */
+router.post('/post/', function (req, res, next) {
+    res.header("Content-Type", 'application/json');
+    res.status(404);
+    res.send(JSON.stringify({
+        error: 'Benutzer konnte nicht angelegt werden.',
+        requirements: ">8 character, 1 Upper Case, 1 Lower Case, 1 Digit, 1 Sign",
+        warning: "Your e-mail address may be in use already."
+    }, 0, 5));
 });
 
 module.exports = router;
